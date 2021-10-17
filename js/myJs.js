dc626325d2c69377e25d4cad0ba169331b15f94a
ton993t <75293729+ton993t@users.noreply.github.com>
2021-10-17 20:25:58 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-              window.location = "https://facebook.com/nganbaochaunguyen";
+              window.location.assign ("https://facebook.com/nganbaochaunguyen");
           },
         });
       }
